ae0f149b17ab51b5a5c17d27f12146f676f2c1ab
steven.allison <steven.allison@CRO-ICG04-SA>
2018-04-25 11:56:20 +0100

    Minor edits.

--- Calculus1/Form1.cs
+++ Calculus1/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Calculus1
 {
     public partial class Form1 : Form
     {
+        class row
+        {
+            public double time;
+            public double voltage;
+            public double current;
+            public double voltageDerivative;
+            public double charge;
+        }
+
+        List<row> table = new List<row>();
+            
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void tableSort()
+        {
+            table = table.OrderBy(x => x.time).ToList();
+        }
+
+        void derivative()
+        {
+            for (int i=1; i < table.Count; i++)
+            {
+                double dV = table[i].voltage - table[i - 1].voltage;
+                double dt = table[i].time - table[i - 1].time;
+                table[i].voltageDerivative = dV / dt;
+            }
+        }
+            
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "CSV Files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new row());
+                            string[] l = sr.ReadLine().Split(',');
+                            table.Last().time = double.Parse(l[0]);
+                            table.Last().voltage = double.Parse(l[1]);
+                            table.Last().current = double.Parse(l[2]);
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " failed to open.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format.");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                }
+            }
         }
     }
 }
