cc3d73428bde1049464ff8c61f36315eb457fef2
steven.allison <steven.allison@CRO-ICG04-SA>
2018-05-02 12:00:47 +0100

    Added Graphs menu - needs work

--- Calculus1/Form1.Designer.cs
+++ Calculus1/Form1.Designer.cs
@@ -28,11 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dVdtTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -42,7 +51,8 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.graphsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
@@ -60,21 +70,70 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // graphsToolStripMenuItem
+            // 
+            this.graphsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vTToolStripMenuItem,
+            this.itToolStripMenuItem,
+            this.dVdtTToolStripMenuItem});
+            this.graphsToolStripMenuItem.Name = "graphsToolStripMenuItem";
+            this.graphsToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.graphsToolStripMenuItem.Text = "Graphs";
+            // 
+            // vTToolStripMenuItem
+            // 
+            this.vTToolStripMenuItem.Name = "vTToolStripMenuItem";
+            this.vTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vTToolStripMenuItem.Text = "V - t";
+            this.vTToolStripMenuItem.Click += new System.EventHandler(this.vTToolStripMenuItem_Click);
+            // 
+            // itToolStripMenuItem
+            // 
+            this.itToolStripMenuItem.Name = "itToolStripMenuItem";
+            this.itToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.itToolStripMenuItem.Text = "I - t";
+            this.itToolStripMenuItem.Click += new System.EventHandler(this.itToolStripMenuItem_Click);
+            // 
+            // dVdtTToolStripMenuItem
+            // 
+            this.dVdtTToolStripMenuItem.Name = "dVdtTToolStripMenuItem";
+            this.dVdtTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dVdtTToolStripMenuItem.Text = "dV/dt - t";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(50, 54);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(585, 355);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +145,11 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ToolStripMenuItem graphsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dVdtTToolStripMenuItem;
     }
 }
 
--- Calculus1/Form1.cs
+++ Calculus1/Form1.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Calculus1
 {
     public partial class Form1 : Form
     {
+        
         class row
         {
             public double time;
@@ -23,11 +25,11 @@ namespace Calculus1
         }
 
         List<row> table = new List<row>();
-            
-
+                
         public Form1()
         {
             InitializeComponent();
+            chart1.Series.Clear();
         }
 
         void tableSort()
@@ -37,7 +39,7 @@ namespace Calculus1
 
         void derivative()
         {
-            for (int i=1; i < table.Count; i++)
+            for (int i=0; i < table.Count; i++)
             {
                 double dV = table[i].voltage - table[i - 1].voltage;
                 double dt = table[i].time - table[i - 1].time;
@@ -79,6 +81,54 @@ namespace Calculus1
                 {
                     MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
                 }
+            }
+        }
+
+        private void vTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;            
+            Series series1 = new Series
+            {
+                Name = "Spline",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+            };
+
+            chart1.Series.Add(series1);
+            for (int i = 0; i < table.Count; i++)
+            {
+                series1.Points.AddXY(table[i].time, table[i].voltage);
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            
+            Series series1 = new Series
+            {
+                Name = "Spline",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+            };
+
+            chart1.Series.Add(series1);
+            for (int i = 0; i < table.Count; i++)
+            {
+                series1.Points.AddXY(table[i].time, table[i].current);
             }
         }
     }
